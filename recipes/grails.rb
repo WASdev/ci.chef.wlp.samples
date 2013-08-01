2933694ddc2dca010854ac47dcf70ce389201659
Jarek Gawor <jgawor@gmail.com>
2013-08-01 17:06:37 -0400

    use wlp_server LWRP to start the sample

--- recipes/grails.rb
+++ recipes/grails.rb
@@ -1,4 +1,10 @@
+serverName = "GrailsSample"
+
 install_sample "grails" do
   location "osi/grails/GrailsSample.jar"
-  server_name "GrailsSample"
+  server_name serverName
+end
+
+wlp_server serverName do
+  action :start
 end
