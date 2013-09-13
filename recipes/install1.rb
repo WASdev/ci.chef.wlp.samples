99a2146a655c59a000120af63715bd1eeb9d9c4a
ant <ant.elder@uk.ibm.com>
2013-09-13 14:41:58 +0100

    Don't specify the application location now that its worked out by default

--- recipes/install1.rb
+++ recipes/install1.rb
@@ -3,7 +3,7 @@
 
 serverName = "Test1"
 
-application "HelloworldApp" do
+application "HelloworldApp2" do
 
   path "/usr/local/helloworldApp"
   repository "http://central.maven.org/maven2/org/apache/tuscany/sca/samples/helloworld-webapp/2.0/helloworld-webapp-2.0.war"
@@ -15,12 +15,10 @@ application "HelloworldApp" do
 
     features [ "jsp-2.2", "servlet-3.0" ]
 
-    # TODO: derive app_location automatically
-    application_location "/usr/local/helloworldApp/current/helloworld-webapp-2.0.war"
-
 #   application name and location default to above "HelloworldApp" and path
 #   config other application attributes via attributes here, eg:
 #      application_type "war"
+#      application_location "/some/file"
 #      application_context_root "someroot"
 #      application_auto_start "true"
 #   or, to use a custom xml template from this cookbook:
