322740889827b130821dc2c92713749802d045b7
ant <ant.elder@uk.ibm.com>
2013-09-17 11:48:50 +0100

    Add new sample recipe showing application_wlp installing the Geronimo JSPExamples app

---
+++ recipes/JSPExamples.rb
@@ -0,0 +1,15 @@
+application "JSPExamples" do
+
+  repository "http://central.maven.org/maven2/org/apache/geronimo/samples/jsp-examples-war/3.0-M1/jsp-examples-war-3.0-M1.war"
+  path "/usr/local/JSPExamples"
+  scm_provider Chef::Provider::RemoteFile::Deploy
+  owner "wlp"
+  group "wlp-admin"
+
+  wlp_application do
+    server_name "JSPExamples"
+    features [ "jsp-2.2", "servlet-3.0" ]
+  end
+
+end
+
