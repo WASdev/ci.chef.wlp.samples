181514ce1ef448ce7ea93bc8882aa6a42ebca04d
Ant Elder <ant.elder@uk.ibm.com>
2014-08-19 11:40:21 +0100

    Add sample recipes for creating Liberty collective controllers and collective members

---
+++ recipes/controller.rb
@@ -0,0 +1,42 @@
+# Recipe that demonstrates how to create a Liberty collective controller
+
+serverName = "controllerA"
+
+# define service beforehand - otherwise notifications from ruby_block won't work
+service "wlp-#{serverName}" do
+  supports :start => true, :restart => true, :stop => true, :status => true
+  action :nothing
+end
+
+wlp_server serverName do
+  config ({
+            "featureManager" => {
+              "feature" => [ "adminCenter-1.0" ]
+            },
+
+            # this file is created by the wlp_collective create
+            "include" => {
+              "location" => "${server.config.dir}/controller.xml"
+            },
+            "httpEndpoint" => {
+              "id" => "defaultHttpEndpoint",
+              "host" => "*",
+              "httpPort" => "9080",
+              "httpsPort" => "9443"
+            }
+          })
+  action :create
+end
+
+wlp_collective "#{serverName}" do
+   action :create
+   server_name serverName
+   keystorePassword "Liberty"
+   admin_user "admin"
+   admin_password "adminpwd"
+end
+
+# start service
+wlp_server serverName do
+  action :start
+end
---
+++ recipes/member.rb
@@ -0,0 +1,56 @@
+# Recipe that demonstrates how to join a server to a Liberty collective
+
+serverName = "memberD"
+
+# define service beforehand - otherwise notifications from ruby_block won't work
+service "wlp-#{serverName}" do
+  supports :start => true, :restart => true, :stop => true, :status => true
+  action :nothing
+end
+
+wlp_server serverName do
+  config ({
+            "featureManager" => {
+              "feature" => [ "jsp-2.2", "clusterMember-1.0" ]
+            },
+
+            # this overrides the defaultCluster name 
+            "clusterMember" => {
+              "name" => [ "myTestCluster" ]
+            },
+
+            # this enables application and packages deploys from the collective 
+            "remoteFileAccess" => {
+              "writeDir" => [ "${server.config.dir}" ]
+            },
+
+            "include" => {
+              # this file is created by the wlp_collective join
+              "location" => "${server.config.dir}/collective-join-include.xml"
+            },
+
+            "httpEndpoint" => {
+              "id" => "defaultHttpEndpoint",
+              "host" => "*",
+              "httpPort" => "9089",
+              "httpsPort" => "9452"
+            }
+          })
+  action :create
+end
+
+# join the server to the collective
+wlp_collective "#{serverName}" do
+   host "localhost"
+   port "9443"
+   user "admin"
+   password "adminpwd"
+   keystorePassword "Liberty"
+   server_name serverName
+   action :join
+end
+
+# start service
+wlp_server serverName do
+  action :start
+end
